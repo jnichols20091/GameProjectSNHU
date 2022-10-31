4d622134940cb82ce3a403da5a2c70c2002ea591
Josh Nichols <jnichols20091@gmail.com>
2022-10-31 00:35:03 -0500

    matrix

--- introscreen.py
+++ introscreen.py
@@ -3,23 +3,36 @@ from asciimatics.screen import *
 from asciimatics.scene import *
 from asciimatics.renderers import *
 from asciimatics.effects import *
+from asciimatics.widgets import *
+import sys
 
 
 
 
-def demo(screen):
+
+def background(screen):
     effects = [
         Cycle(
             screen,
-            FigletText("Welcome!!!", font='big'),
+            FigletText("Welcome!!!", font='ogre'),
             screen.height // 2 - 3),
         Cycle(
             screen,
-            FigletText("To The Game!", font='big'),
+            FigletText("To The Game!", font='ogre'),
             screen.height // 2 + 3),
-        Stars(screen, (screen.width + screen.height))
+        Stars(screen, (screen.width + screen.height)),
+        Matrix(
+            screen,
+        )
     ]
     screen.play([Scene(effects, 500)])
 
-Screen.wrapper(demo)
 
+def global_shortcuts(event):
+    if isinstance(event, KeyboardEvent):
+        c = event.key_code
+        if c in (17, 24):
+            raise StopApplication("User Terminated app")
+
+
+Screen.wrapper(background)
