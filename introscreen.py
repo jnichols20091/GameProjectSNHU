81fd005bde5adf20520fc1f544202aa32c2528a8
jnichols20091 <jnichols20091@gmail.com>
2022-10-30 22:41:04 -0500

    introscreenstarstext

--- introscreen.py
+++ introscreen.py
@@ -1,5 +1,7 @@
 import asciimatics.effects
 from asciimatics.screen import *
+from asciimatics.scene import *
+from asciimatics.renderers import *
 from asciimatics.effects import *
 
 
@@ -7,7 +9,17 @@ from asciimatics.effects import *
 
 def demo(screen):
     effects = [
-
+        Cycle(
+            screen,
+            FigletText("Welcome!!!", font='big'),
+            screen.height // 2 - 3),
+        Cycle(
+            screen,
+            FigletText("To The Game!", font='big'),
+            screen.height // 2 + 3),
+        Stars(screen, (screen.width + screen.height))
+    ]
+    screen.play([Scene(effects, 500)])
 
 Screen.wrapper(demo)
 
