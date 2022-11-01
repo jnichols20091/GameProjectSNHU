01e9d9723e5d29e682934bc925e10bffcba79cdd
jnichols20091 <jnichols20091@gmail.com>
2022-10-31 23:03:35 -0500

    Halloween

--- introscreen.py
+++ introscreen.py
@@ -1,38 +1,32 @@
+import math
+
 import asciimatics.effects
+from asciimatics.paths import Path
 from asciimatics.screen import *
 from asciimatics.scene import *
 from asciimatics.renderers import *
 from asciimatics.effects import *
-from asciimatics.widgets import *
+from asciimatics.sprites import *
 import sys
-
-
-
 
 
 def background(screen):
     effects = [
         Cycle(
             screen,
-            FigletText("Welcome!!!", font='ogre'),
-            screen.height // 2 - 3),
+            FigletText("Welcome", font='ogre'),
+            screen.height // 4 - 3),
         Cycle(
             screen,
             FigletText("To The Game!", font='ogre'),
-            screen.height // 2 + 3),
+            screen.height // 4 + 3),
         Stars(screen, (screen.width + screen.height)),
         Matrix(
             screen,
-        )
+        ),
     ]
+
     screen.play([Scene(effects, 500)])
-
-
-def global_shortcuts(event):
-    if isinstance(event, KeyboardEvent):
-        c = event.key_code
-        if c in (17, 24):
-            raise StopApplication("User Terminated app")
 
 
 Screen.wrapper(background)
